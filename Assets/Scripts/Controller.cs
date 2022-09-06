c3ce78d1d25fde11f1b897fabb5a7c775cadcd00
steve rector <steve.rector@datadoghq.com>
2022-09-05 17:11:37 -0700

    Adds new input system and sets up player attack

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -1,18 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Controller : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private PlayerControls playerControls;
+
+    private void Awake()
     {
-        
+        playerControls = new PlayerControls();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        playerControls.Enable();
+        playerControls.Player.Attack.performed += Attack;
+    }
+
+    private void OnDisable()
+    {
+        playerControls.Disable();
+        playerControls.Player.Attack.performed += Attack;
+    }
+
+    private void Attack(InputAction.CallbackContext obj)
+    {
+        Debug.Log("Attacking");
+        Debug.Log(obj);
     }
 }
