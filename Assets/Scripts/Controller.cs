fe4067cd7fad1aabde0cdd1a6dce6e7951f2a18f
steve rector <steve.rector@datadoghq.com>
2022-09-05 17:45:45 -0700

    Adds Index to each new controller added
    keeps track of number of players in the game

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -13,12 +13,16 @@ public class Controller : MonoBehaviour
         playerControls = new PlayerControls();
     }
 
+    private void Start()
+    {
+        index = FindObjectsOfType<Controller>().Length;
+    }
+
     private void OnEnable()
     {
         playerControls.Enable();
         playerControls.Player.Attack1.performed += Attack1;
         playerControls.Player.Attack2.performed += Attack2;
-
     }
 
     private void OnDisable()
@@ -26,13 +30,11 @@ public class Controller : MonoBehaviour
         playerControls.Disable();
         playerControls.Player.Attack1.performed -= Attack1;
         playerControls.Player.Attack2.performed -= Attack2;
-
     }
 
     private void Attack1(InputAction.CallbackContext obj)
     {
         Debug.Log("Attacking with Attack1");
-        Debug.Log(obj);
     }
 
     private void Attack2(InputAction.CallbackContext obj)
